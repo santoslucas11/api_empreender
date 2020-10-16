2a66645135f2e6b64d98ddfb2ee6506612bd6f87
lucas <santoslucas.dev@gmail.com>
2020-10-16 18:08:02 -0300

    Implementação de database com typer orm e sqlite3, criação da primeira migration

---
+++ src/database/migrations/1602882275533-create_empreendedores.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createEmpreendedores1602882275533 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
