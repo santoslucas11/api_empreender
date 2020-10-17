0aeefe881dc42bd2ecb5b304964ecc763e04b5ea
lucas <santoslucas.dev@gmail.com>
2020-10-16 21:08:56 -0300

    upload de imagens para o banco

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 
 import { Router }from 'express';
 import EmpreendedoresController from './controllers/EmpreendedoresController';
+import multer from 'multer';
+
+import  uploadConfig from './config/upload';
+
 const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.get('/empreendedores', EmpreendedoresController.index);
 routes.get('/empreendedores/:id', EmpreendedoresController.show);
-routes.post('/empreendedores', EmpreendedoresController.create);
+routes.post('/empreendedores', upload.array('images') ,EmpreendedoresController.create);
 
 export default routes;
