4afa80f188f9534ecac3ef7b7f17041b311e14b3
lucas <santoslucas.dev@gmail.com>
2020-10-16 23:14:03 -0300

    Finalizando API
    Tratativas para os erros 500 e 400, instalacção da dependência cors para que o back end converse com o front end.

--- src/controllers/EmpreendedoresController.ts
+++ src/controllers/EmpreendedoresController.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from 'express';
 import { getRepository } from 'typeorm';
 import empreendedorView from '../views/empreendedor_view';
+import * as Yup from 'yup' ;
 
 import Empreendedores from '../models/Empreendedor';
 
@@ -48,7 +49,7 @@ export default{
             return{ path: image.filename }
         })
 
-        const empreendedor = empreendedoresRepository.create({
+        const data = {
             name,
             latitude,
             longitude,
@@ -57,7 +58,28 @@ export default{
             opening_hours,
             open_on_weekends,
             images
+        }
+
+        const schema = Yup.object().shape({
+            nome: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(
+                Yup.object().shape({
+                    path: Yup.string().required()
+                })
+            )
         });
+
+        await schema.validate(data, {
+            abortEarly: false,
+        });
+
+        const empreendedor = empreendedoresRepository.create(data);
     
         await empreendedoresRepository.save(empreendedor);
 
