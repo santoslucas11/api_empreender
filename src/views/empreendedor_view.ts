c0aee01799ad6808ba4b6ea062ab9b34954f5085
lucas <santoslucas.dev@gmail.com>
2020-10-16 21:56:54 -0300

    Finalizando upload de imagens.

--- src/views/empreendedor_view.ts
+++ src/views/empreendedor_view.ts
@@ -1,4 +1,5 @@
 import Empreendedor from '../models/Empreendedor';
+import imagesView   from './images_view';
 
 export default{
     render(empreendedor: Empreendedor){
@@ -11,6 +12,7 @@ export default{
             instructions: empreendedor.instructions,
             opening_hours: empreendedor.opening_hours,
             open_on_weekends: empreendedor.open_on_weekends,
+            images: imagesView.renderMany(empreendedor.images)
         };
     },
 
